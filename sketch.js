ab79221c40a8d7413fa7c2a22384f49d4543758e
saragupta2008 <74290865+saragupta2008@users.noreply.github.com>
2021-01-15 16:59:57 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -3,23 +3,58 @@ var dog,dogImg,dogImg2,  database, foodS, foodStock;
 function preload()
 {
   //load images here
-  dogImg=loadImage("dogImg.png");
-  dogImg2=loadImage("dogImg1.png")
+  dogImg=loadImage("images/dogImg.png");
+  dogImg2=loadImage("images/dogImg1.png")
 }
 
 function setup() {
   createCanvas( 500, 500);
-  dog=createSprite(200,100);
+  database=firebase.database();
+  dog=createSprite(300,300);
   dog.addImage(dogImg);
+  dog.scale=0.2
+
+  foodStock=database.ref('food');
+  foodStock.on("value",readStock);
+  
   
 }
 
 
 function draw() {  
+background(46, 139, 87)
+if(keyWentDown(UP_ARROW)){
+writeStock(foodS);
+dog.addImage(dogImg2);
+}
 
   drawSprites();
   //add styles here
+  textSize(40)
+  fill (0)
+  text ("Food Remaining: "+foodS,100,60)
+textSize(20)
+  text("Note:Press UP Arrow key to feed Drago milk!",70,90)
 
+}
+
+function readStock(data){
+  foodS=data.val();
+}
+function writeStock(x,){
+
+  if(x<=0){
+    x=0
+  }
+  else{
+    x=x-1
+  }
+  database.ref('/').update(
+      {
+         food:x
+      }
+  )
+  
 }
 
 
